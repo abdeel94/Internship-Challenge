b970d5925635a8399be3fec540e824ea0452bd2b
abdeel94 <abdeel4991@hotmail.com>
2023-03-01 19:08:27 -0300

    initiliaze

--- src/internshipchallenge/Lista.java
+++ src/internshipchallenge/Lista.java
@@ -7,7 +7,7 @@ package internshipchallenge;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+// import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
